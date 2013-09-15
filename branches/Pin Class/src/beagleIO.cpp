d20cff31bda4c65876f88697ccad02a8c46c9271
daniel@deathbylogic.com <daniel@deathbylogic.com@225b86b6-e784-5cc2-4f20-715d8fb28edb>
2013-09-15 19:57:09 +0000

    Updated definition of pin objects; Added license to file header.

--- branches/Pin Class/src/beagleIO.cpp
+++ branches/Pin Class/src/beagleIO.cpp
@@ -1,19 +1,108 @@
 /*
- * beagleGPIO.cpp
- *
- *  Created on: Mar 24, 2013
- *      Author: daniel@deathbylogic.com
+	Copyright (C) 2013 deathbylogic.com
+	Author: daniel@deathbylogic.com
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "beagleIO.h"
+
+/*
+ * P8 Header Pins
  */
 
-#include <unistd.h>
-#include <iostream>
-#include <typeinfo>
+beagleDigital P8_03("GPIO1_6",		38);
+beagleDigital P8_04("GPIO1_7",		39);
+beagleDigital P8_05("GPIO1_2",		34);
+beagleDigital P8_06("GPIO1_3",		35);
+beagleDigital P8_07("TIMER4",		66);
+beagleDigital P8_08("TIMER7",		67);
+beagleDigital P8_09("TIMER5",		69);
+beagleDigital P8_10("TIMER6",		68);
+beagleDigital P8_11("GPIO1_13",		45);
+beagleDigital P8_12("GPIO1_12",		44);
+beagleDigital P8_13("EHRPWM2B",		23);
+beagleDigital P8_14("GPIO0_26",		26);
+beagleDigital P8_15("GPIO1_15",		47);
+beagleDigital P8_16("GPIO1_14",		46);
+beagleDigital P8_17("GPIO0_27",		27);
+beagleDigital P8_18("GPIO2_1",		65);
+beagleDigital P8_19("EHRPWM2A",		22);
+beagleDigital P8_20("GPIO1_31",		63);
+beagleDigital P8_21("GPIO1_30",		62);
+beagleDigital P8_22("GPIO1_5",		37);
+beagleDigital P8_23("GPIO1_4",		36);
+beagleDigital P8_24("GPIO1_1",		33);
+beagleDigital P8_25("GPIO1_0",		32);
+beagleDigital P8_26("GPIO1_29",		61);
+beagleDigital P8_27("GPIO2_22",		86);
+beagleDigital P8_28("GPIO2_24",		88);
+beagleDigital P8_29("GPIO2_23",		87);
+beagleDigital P8_30("GPIO2_25",		89);
+beagleDigital P8_31("UART5_CTSN",	10);
+beagleDigital P8_32("UART5_RTSN",	11);
+beagleDigital P8_33("UART4_RTSN",	9);
+beagleDigital P8_34("UART3_RTSN",	81);
+beagleDigital P8_35("UART4_CTSN",	8);
+beagleDigital P8_36("UART3_CTSN",	80);
+beagleSerial  P8_37("UART5_TXD",	78, 	"ttyO5");
+beagleSerial  P8_38("UART5_RXD",	79,		"ttyO5");
+beagleDigital P8_39("GPIO2_12",		76);
+beagleDigital P8_40("GPIO2_13",		77);
+beagleDigital P8_41("GPIO2_10",		74);
+beagleDigital P8_42("GPIO2_11",		75);
+beagleDigital P8_43("GPIO2_8",		72);
+beagleDigital P8_44("GPIO2_9",		73);
+beagleDigital P8_45("GPIO2_6",		70);
+beagleDigital P8_46("GPIO2_7",		71);
 
-#include "beagleGPIO.h"
-//#include "beagleDigital.h"
-#include "beagleSerial.h"
+/*
+ * P9 Header Pins
+ */
 
-using namespace std;
+beagleSerial  P9_11("UART4_RXD",	30,		"ttyO4");
+beagleDigital P9_12("GPIO1_28",		60);
+beagleSerial  P9_13("UART4_TXD",	31,		"ttyO4");
+beagleDigital P9_14("EHRPWM1A",		50);
+beagleDigital P9_15("GPIO1_16",		48);
+beagleDigital P9_16("EHRPWM1B",		51);
+beagleDigital P9_17("I2C1_SCL",		5);
+beagleDigital P9_18("I2C1_SDA",		4);
+beagleDigital P9_19("I2C2_SCL",		13);
+beagleDigital P9_20("I2C2_SDA",		12);
+beagleSerial  P9_21("UART2_TXD",	3,		"ttyO2");
+beagleSerial  P9_22("UART2_RXD",	2,		"ttyO2");
+beagleDigital P9_23("GPIO1_17",		49);
+beagleSerial  P9_24("UART1_TXD",	15,		"ttyO1");
+beagleDigital P9_25("GPIO3_21",		88);
+beagleSerial  P9_26("UART1_RXD",	14,		"ttyO1");
+beagleDigital P9_27("GPIO3_19",		86);
+beagleDigital P9_28("SPI1_CS0",		84);
+beagleDigital P9_29("SPI1_D0",		82);
+beagleDigital P9_30("SPI1_D1",		116);
+beagleDigital P9_31("SPI1_SCLK",	110);
+// Pin P9_32 is VDD_ADC
+beagleAnalog  P9_33("AIN4",			"ANI4");
+// Pin P9_34 is GNDA_ADC
+beagleAnalog  P9_35("AIN6",			"ANI6");
+beagleAnalog  P9_36("AIN5",			"ANI5");
+beagleAnalog  P9_37("AIN2",			"ANI2");
+beagleAnalog  P9_38("AIN3",			"ANI3");
+beagleAnalog  P9_39("AIN0",			"ANI0");
+beagleAnalog  P9_40("AIN1",			"ANI1");
+beagleDigital P9_41("CLKOUT2",		20);
+beagleDigital P9_42("GPIO0_7",		7);
 
 // I/O Header Pins
 //io_pin P8_pins[46] = {
@@ -111,12 +200,6 @@ using namespace std;
 //	{"GND",			0,		POWER,				NULL,				NULL		},
 //	{"GND",			0,		POWER,				NULL,				NULL		},
 //	{"GND",			0,		POWER,				NULL,				NULL		}};
-//
-//io_pin usr_pins[4] = {
-//	{"USR0",		53,		DIGITAL,			"gpmc_a5",			NULL		},
-//	{"USR1",		54,		DIGITAL,			"gpmc_a6",			NULL		},
-//	{"USR2",		55,		DIGITAL,			"gpmc_a7",			NULL		},
-//	{"USR3",		56,		DIGITAL,			"gpmc_a8",			NULL		}};
 
 // BeagleIO objects for P8 & P9 connectors
 //beagleGPIO P8(P8_pins, 46);
@@ -127,5 +210,4 @@ using namespace std;
 //beagleSerial serial2("/dev/ttyO2");
 //beagleSerial serial4("/dev/ttyO4");
 //beagleSerial serial5("/dev/ttyO5");
-
 
--- branches/Pin Class/src/beagleIO.h
+++ branches/Pin Class/src/beagleIO.h
@@ -1,25 +1,112 @@
 /*
- * beagleGPIO.h
- *
- *  Created on: Feb 11,	2013
- *      Author: daniel@deathbylogic.com
- */
+	Copyright (C) 2013 deathbylogic.com
+	Author: daniel@deathbylogic.com
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef BEAGLEIO_H_
 #define BEAGLEIO_H_
 
-#include "beagleGPIO.h"
-//#include "beagleDigital.h"
+#include "beagleAnalog.h"
+#include "beagleDigital.h"
 #include "beagleSerial.h"
 
-//extern beagleDigital *J8P39;
+/*
+ * P8 Header Pins
+ */
 
-//extern beagleGPIO P8;
-//extern beagleGPIO P9;
-//
-//extern beagleSerial serial1;
-//extern beagleSerial serial2;
-//extern beagleSerial serial4;
-//extern beagleSerial serial5;
+extern beagleDigital P8_03;
+extern beagleDigital P8_04;
+extern beagleDigital P8_05;
+extern beagleDigital P8_06;
+extern beagleDigital P8_07;
+extern beagleDigital P8_08;
+extern beagleDigital P8_09;
+extern beagleDigital P8_10;
+extern beagleDigital P8_11;
+extern beagleDigital P8_12;
+extern beagleDigital P8_13;
+extern beagleDigital P8_14;
+extern beagleDigital P8_15;
+extern beagleDigital P8_16;
+extern beagleDigital P8_17;
+extern beagleDigital P8_18;
+extern beagleDigital P8_19;
+extern beagleDigital P8_20;
+extern beagleDigital P8_21;
+extern beagleDigital P8_22;
+extern beagleDigital P8_23;
+extern beagleDigital P8_24;
+extern beagleDigital P8_25;
+extern beagleDigital P8_26;
+extern beagleDigital P8_27;
+extern beagleDigital P8_28;
+extern beagleDigital P8_29;
+extern beagleDigital P8_30;
+extern beagleDigital P8_31;
+extern beagleDigital P8_32;
+extern beagleDigital P8_33;
+extern beagleDigital P8_34;
+extern beagleDigital P8_35;
+extern beagleDigital P8_36;
+extern beagleSerial  P8_37;
+extern beagleSerial  P8_38;
+extern beagleDigital P8_39;
+extern beagleDigital P8_40;
+extern beagleDigital P8_41;
+extern beagleDigital P8_42;
+extern beagleDigital P8_43;
+extern beagleDigital P8_44;
+extern beagleDigital P8_45;
+extern beagleDigital P8_46;
+
+/*
+ * P9 Header Pins
+ */
+
+extern beagleSerial  P9_11;
+extern beagleDigital P9_12;
+extern beagleSerial  P9_13;
+extern beagleDigital P9_14;
+extern beagleDigital P9_15;
+extern beagleDigital P9_16;
+extern beagleDigital P9_17;
+extern beagleDigital P9_18;
+extern beagleDigital P9_19;
+extern beagleDigital P9_20;
+extern beagleSerial  P9_21;
+extern beagleSerial  P9_22;
+extern beagleDigital P9_23;
+extern beagleSerial  P9_24;
+extern beagleDigital P9_25;
+extern beagleSerial  P9_26;
+extern beagleDigital P9_27;
+extern beagleDigital P9_28;
+extern beagleDigital P9_29;
+extern beagleDigital P9_30;
+extern beagleDigital P9_31;
+// Pin P9_32 is VDD_ADC
+extern beagleAnalog  P9_33;
+// Pin P9_34 is GNDA_ADC
+extern beagleAnalog  P9_35;
+extern beagleAnalog  P9_36;
+extern beagleAnalog  P9_37;
+extern beagleAnalog  P9_38;
+extern beagleAnalog  P9_39;
+extern beagleAnalog  P9_40;
+extern beagleDigital P9_41;
+extern beagleDigital P9_42;
 
 #endif /* BEAGLEIO_H_ */
